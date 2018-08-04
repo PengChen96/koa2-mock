c756324155238d0c1c28de41761bde66c16c6b0f
cpg <cpg@servyou.com.cn>
2018-08-04 19:23:33 +0800

    2018/8/4

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('koa-router')();
+const fs = require('fs');
 const cors = require('koa2-cors');
 app.use(cors());
 
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, world!</h1>';
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
-    await next();
+  ctx.response.type = 'text/html';
+  ctx.response.body = `<h1>Hello, world!</h1>`;
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
+  // 读取文件
+  await new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/json/test.json`, 'utf8', (err, data) => {
+      if (err) {
+        resolve('error');
+      } else {
+        resolve(data.toString());
+      }
+    })
+  }).then((result) => {
+    const da = JSON.parse(JSON.stringify(result));
+    console.log(typeof da);
+    ctx.response.body = result.toString();
+  });
+  await next();
 });
 
 router.get('/person/info', async (ctx, next) => {
-    ctx.response.body = {
-        name: '张三',
-        age: '18'
-    };
+  ctx.response.body = {
+    name: '张三',
+    gender: '男'
+  };
 });
+
+router.get('/read', async (ctx, next) => {
+  await new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/json/test.json`, (err, data) => {
+      if (err) {
+        resolve('error');
+      } else {
+        resolve(data.toString());
+      }
+    })
+  }).then((result) => {
+    ctx.response.body = result;
+  });
+});
+
 
 app.use(router.routes());
 
@@ -25,4 +55,8 @@ taxController = require('./controllers/tax');
 app.use(taxController.routes());
 
 app.listen(3000);
-console.log('app started at port 3000...')
+console.log('app started at port 3000...');
+
+const result = fs.readFileSync(`${__dirname}/json/test.json`).toString();
+const b = JSON.parse(result);
+console.log(b.name);
